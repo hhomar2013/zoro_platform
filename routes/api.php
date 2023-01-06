80234337cbcd8e6f6f8be899dbf8da25de90ecb3
hhomar2013 <hhomar2013@gmail.com>
2023-01-06 03:59:58 +0200

    my edit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user_profile',[AuthController::class,'user_profile']);
+    Route::post('user/revoke',[AuthController::class,'revoke']);
 });
